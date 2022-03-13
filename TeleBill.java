536b11a1e3125b9de9f802d1b8b85ed6b1081779
Yash-Daxini <100989835+Yash-Daxini@users.noreply.github.com>
2022-03-13 17:50:22 +0530

    Update TeleBill.java

--- TeleBill.java
+++ TeleBill.java
@@ -4,26 +4,21 @@ class TeleBill{
         System.out.print("ENTER NUMBER OF CALLS:");
         Scanner sc = new Scanner(System.in);
         int call = sc.nextInt();
-        float amount = 0f;
-        if(call > 200){
-            amount += (call-200)*0.4;
-             call = call - 200;
-            System.out.print(call);
-        }
-        else if(call > 150 && call <= 200){
-            amount += (call-50)*0.5;
-            call = call - 50;
-            System.out.print(call);
-        }
-        else if(call > 100 && call <= 150){
-            call = call - 50;
-            amount += (call-50)*0.6;
-            System.out.print(call);
-        }
-        else if(call <= 100){
+        double amount = 0;
+        if(call < 100){
             amount = 200;
-            System.out.print(call);
         }
-        System.out.print("Monthly telephone bill = "+amount);
+        else if(call >= 100  && call < 150){
+            amount = (call - 100)*0.6 + 200;
+            call = 100;
+        }
+        else if(call >= 150 && call < 200){
+            amount = (call - 150)*1.1 + 50 * 0.6 + 200;
+            call = 150;
+        }
+        else if(call >= 200){
+            amount = (call - 200) * 0.4+ 50 * 0.5 + 50 * 0.6 + 200;
+        }
+        System.out.print("Monthly telephone bill ="+amount);
     }
 }
